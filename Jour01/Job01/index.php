105d6a9805fe915e33a4014cc11c0019b6076822
JadeMercante <maxim.mercante@laplateforme.io>
2024-05-21 14:16:48 +0200

    Fix

---
+++ Jour01/Job01/index.php
@@ -0,0 +1,15 @@
+<?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+$str = $str2 . " " . $str . " " . $str3;
+echo($str);
+$val = 6;
+echo ($val);
+$val = $val + 4;
+echo ($val);
+$myBool = true;
+echo ($myBool);
+$myBool = false;
+echo ($myBool);
+?>
