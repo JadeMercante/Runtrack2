8aa2dcb0ab841a748730777175c8d10bdd77922b
JadeMercante <maxim.mercante@laplateforme.io>
2024-05-21 15:51:18 +0200

    Tip tow

---
+++ Jour01/Job02/index.php
@@ -0,0 +1,4 @@
+<?php
+/*“LaPlateforme”*/
+//LaPlateforme
+?>
---
+++ Jour02/Job02/index.php
@@ -0,0 +1,24 @@
+<?php
+$Nombre = 0;
+while($Nombre < 1338) {
+    if ($Nombre == 26) {
+        echo "<br />";
+    }
+    elseif ($Nombre == 37) {
+        echo "<br />";
+    }
+    elseif ($Nombre == 88) {
+        echo "<br />";
+    }
+    elseif ($Nombre == 1111) {
+        echo "<br />";
+    }
+    elseif ($Nombre == 3233) {
+        echo "<br />"; //Jsp pk, dans tout les cas, sa s'arrete a 1337 ptdr
+    }
+    else {
+        echo $Nombre . "<br />";
+    }
+    $Nombre++;
+}
+?>
